271e39f4906419c43ea9617bbd4651eadf4c0d4f
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-20 19:31:16 -0300

    Contador utilizando Svelte

---
+++ Framework Svelte/template-master/src/Contador.svelte
@@ -0,0 +1,25 @@
+<script>
+    //REAGINDO A MUDANÇAS DE ESTADO
+    let contador = 0;
+
+    function incrementa(){
+        contador++;
+    }
+    function decrementa(){
+        contador--;
+    }
+
+</script>
+
+<div>
+    Valor: { contador } 
+    <button on:click = {incrementa}>Incrementar</button> 
+        <!--Poderia ter utilizado { () => contador++ }-->
+    <button on:click = {decrementa}>Decrementar</button>
+</div>
+
+<style>
+    button{
+        color: blue;
+    }
+</style>
